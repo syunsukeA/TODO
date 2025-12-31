e895fb564d8caaa34134dca7d4b7eae60ae0174b
syunsukeA <shunpeik2@gmail.com>
2025-12-31 18:16:19 +0900

    Todo一覧を取得するAPIの実装

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class TodosController < BaseController
+      DEFAULT_LIMIT = 20
+      MAX_LIMIT = 100
+
       def create
         todo = Todo.new(todo_params)
 
@@ -11,10 +14,28 @@ module Api
         end
       end
 
+      def index
+        limit = limit_from_params
+        todos = Todo.order(created_at: :desc).limit(limit)
+
+        render json: { todos: todos.as_json(only: %i[id title created_at]) }, status: :ok
+      rescue ArgumentError, TypeError
+        render json: { errors: ["limit must be an integer between 1 and #{MAX_LIMIT}"] }, status: :bad_request
+      end
+
       private
 
       def todo_params
         params.permit(:title)
+      end
+
+      def limit_from_params
+        return DEFAULT_LIMIT if params[:limit].nil?
+
+        value = Integer(params[:limit], 10)
+        raise ArgumentError if value < 1 || value > MAX_LIMIT
+
+        value
       end
     end
   end
